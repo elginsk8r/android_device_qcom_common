748228c06f3e834cc2c39cfe9e9a1c042650de23
TheStrix <parthbhatia98@gmail.com>
2020-03-19 02:35:44 +0530

    common: Add WFD system components
     - To use this, set TARGET_COMMON_QTI_COMPONENTS := <components>.
       e.g. TARGET_COMMON_QTI_COMPONENTS := av bt perf
     - Configurations go inside qti-components/<component>/qti-<component>.mk
     - Blob list go in qti-components/<component>/proprietary-files.txt
    
    Change-Id: I9868b02da59c69326f3587cf338187753955c1db

---
+++ system/wfd/qti-wfd.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libnl \
+    vendor.qti.hardware.display.config-V5-ndk
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    debug.sf.enable_hwc_vds=0 \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0
+
+# Inherit QCOM display dependencies.
+$(call inherit-product-if-exists, vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/qcom/common/system/wfd/wfd-vendor.mk)
