1e3170a68d262a0e9694551d1896c9591a15f4d8
Nick Reuter <nreuter85@gmail.com>
2024-02-25 20:48:02 -0600

    common: generate a makefile for support platforms
    Change-Id: Ie4137ea20ec080c835abf2f1dd8e6f5e6d31d500

---
+++ build/target/sm6150.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The Evervolv Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Platform
+TARGET_BOARD_PLATFORM := sm6150
+
+# Inherit from the proprietary files makefile.
+include device/qcom/common/build/target/common.mk
